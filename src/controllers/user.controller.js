826069f2a0727c4457c380930ba311962faaf0ef
Andres Felipe Rico <andrespro2004@gmail.com>
2025-09-17 23:29:08 -0500

    Integracion de metodo GetUserById

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,4 +31,34 @@ async function createUser(req, res) {
   }
 }
 
-module.exports = { createUser };
+async function getUserById(req, res) {
+  try {
+    const { id } = req.params; // el id lo pasas en la URL, ej: /users/:id
+
+    const docRef = admin.firestore().collection('users').doc(id);
+    const doc = await docRef.get();
+
+    if (!doc.exists) {
+      return res.status(404).json({
+        message: 'Usuario no encontrado'
+      });
+    }
+
+    const data = doc.data();
+
+    // reconstruyo con el modelo si quieres mantener consistencia
+    const user = new User(id, data.name, data.birthdate, data.createdAt);
+
+    return res.status(200).json({
+      message: 'Usuario encontrado',
+      data: user
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error al obtener usuario',
+      error: error.message
+    });
+  }
+}
+
+module.exports = { createUser, getUserById };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createUser } = require('../controllers/user.controller');
+const { createUser, getUserById  } = require('../controllers/user.controller');
 const verifyFirebaseToken = require('../middleware/firebaseAuth');
 
 // Crear usuario (requiere token de Firebase)
 router.post('/', verifyFirebaseToken, createUser);
 
+// Obtener usuario por ID
+router.get('/:id', verifyFirebaseToken, getUserById);
 
 module.exports = router;
